8c993a0a635f58279a6219462b95a481ce963fa6
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2026-08-20 20:29:20 +0100

    feat: implement HomeViewModel and update HomeScreen to display temperature and postcode if not null

---
+++ lib/viewmodels/home_view_model.dart
@@ -0,0 +1,12 @@
+// TODO: Actually implement this
+class HomeViewModel {
+  String? get currentTemperature {
+    // return '16°C';
+    return null;
+  }
+
+  String? get postCode {
+    // return 'AB4 3CD';
+    return null;
+  }
+}
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'package:pollen_app/viewmodels/home_view_model.dart';
 import 'package:pollen_app/widgets/app_navigation_bar.dart';
 import 'package:pollen_app/widgets/home/analysis_card_widget.dart';
 import 'package:pollen_app/widgets/home/live_pollen_tracker_widget.dart';
@@ -8,32 +9,51 @@ import 'package:pollen_app/widgets/home/live_pollen_tracker_widget.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  bool _hasValue(String? value) {
+    return value != null && value.trim().isNotEmpty;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final homeViewModel = HomeViewModel();
+
+    final hasPostCode = _hasValue(homeViewModel.postCode);
+    final hasTemperature = _hasValue(homeViewModel.currentTemperature);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Pollen App')),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Padding(
-              padding: EdgeInsetsDirectional.symmetric(horizontal: 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'DE4 3GY',
-                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
-                  ),
-                  Text(
-                    '16°C',
-                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
-                  ),
-                ],
-              ),
-            ),
+            if (hasPostCode || hasTemperature)
+              Padding(
+                padding: const EdgeInsetsDirectional.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (hasPostCode)
+                      Text(
+                        homeViewModel.postCode!,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 24,
+                        ),
+                      ),
 
-            const SizedBox(height: 16),
+                    if (hasTemperature)
+                      Text(
+                        homeViewModel.currentTemperature!,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+
+            if (hasPostCode || hasTemperature) const SizedBox(height: 16),
 
             const LivePollenTrackerWidget(),
 
@@ -49,37 +69,31 @@ class HomeScreen extends StatelessWidget {
                     icon: Icons.analytics_outlined,
                     onTap: () {
                       if (kDebugMode) {
-                        print("Pollen Analysis clicked");
+                        print('Pollen Analysis clicked');
                       }
                     },
                   ),
+
                   const SizedBox(width: 12),
+
                   AnalysisCardWidget(
                     title: 'Pollen Forecast',
                     icon: Icons.calendar_today_outlined,
                     onTap: () {
                       if (kDebugMode) {
-                        print("Pollen Forecast clicked");
+                        print('Pollen Forecast clicked');
                       }
                     },
                   ),
+
                   const SizedBox(width: 12),
-                  AnalysisCardWidget(
-                    title: 'Weather',
-                    icon: Icons.cloud_outlined,
-                    onTap: () {
-                      if (kDebugMode) {
-                        print("Weather clicked");
-                      }
-                    },
-                  ),
-                  const SizedBox(width: 12),
+
                   AnalysisCardWidget(
                     title: 'Allergy Log',
                     icon: Icons.edit_note_outlined,
                     onTap: () {
                       if (kDebugMode) {
-                        print("Allergy Log clicked");
+                        print('Allergy Log clicked');
                       }
                     },
                   ),
