c12654ecd3c4a74b682d008c9afa3c9b5a62d97e
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2026-08-21 10:49:51 +0100

    feat: add debug banner ad unit IDs for Android and iOS in AdHelper

--- lib/services/ad_helper.dart
+++ lib/services/ad_helper.dart
@@ -1,7 +1,19 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 class AdHelper {
   static String get bannerAdUnitId {
+    if (kDebugMode) {
+      if (Platform.isAndroid) {
+        return 'ca-app-pub-3940256099942544/9214589741';
+      }
+
+      if (Platform.isIOS) {
+        return 'ca-app-pub-3940256099942544/2435281174';
+      }
+    }
+
     if (Platform.isAndroid) {
       return 'ca-app-pub-7173006780619406/3659271844';
     }
